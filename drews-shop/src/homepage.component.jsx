2658cfba253f680d081926cb037f175c9f284f53
Andrew Perez <andrewperez@andrews-air.home>
2020-03-24 12:02:46 -0700

    created homepage

---
+++ drews-shop/src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () =>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Category 1</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Category 2</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Category 3</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Category 4</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>Category 5</h1>
+                <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
