e142ca12ae298e0710637556d06bf3bbfe0020e9
Andrew Perez <andrewperez@andrews-air.home>
2020-04-08 21:29:08 -0700

    adding thunk which dispatches and reads functions to our actions

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,37 @@
-import ShopActionTypes from './shop.types'
+// actions:
+// functions that return object
 
-export const updateCollections = (collectionsMap) => ({
-    type:ShopActionTypes.UPDATE_COLLECTIONS,
+// thunk:
+// functions that return a function that gets dispatch in it
+// so whenever dispatch is called, it fires multiple actions
+// this allows us to dispatch multiple fuctions to handle asynchronous code
+
+import ShopActionTypes from './shop.types'
+import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils'
+
+export const fetchCollectionsStart = () => ({
+    type:ShopActionTypes.FETCH_COLLECTIONS_START
+    // switches the reducers' isFetching state
+})
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type:ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 })
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload:errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart())
+        collectionRef.get().then(
+         snapshot =>{
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+        })
+    }
+}
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,16 +2,30 @@ import SHOP_DATA from './shop.data'
 import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 }
 
 const shopReducer = (currentState = INITIAL_STATE, actions) => {
     switch(actions.type){
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
             return{
                 ...currentState,
+                isFetching:true
+            }
+        case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...currentState,
+                isFetching:false,
                 collections:actions.payload
             }
+            case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+            return {
+                ...currentState,
+                isFetching:false,
+                errorMessage:actions.payload
+            }            
         default:
         return currentState;
     }
